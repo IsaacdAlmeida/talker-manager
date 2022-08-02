9d1fd2da50a0a7fba5bcbc9a15bbb1bcd17b4504
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-02 17:28:19 -0300

    style(eslint): change some lines to fix lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { readFile } = require('./utils/fs-utils')
+const { readFile } = require('./utils/fs-utils');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,7 +10,7 @@ const HTTP_NOT_FOUND = 404;
 const HTTP_INTERNAL_ERROR = 500;
 const PORT = '3000';
 
-app.get ('/talker', async (_req, res) => {
+app.get('/talker', async (_req, res) => {
   const talkerArray = await readFile();  
   return res.status(HTTP_OK_STATUS).json(talkerArray);
 });
@@ -21,7 +21,9 @@ app.get('/talker/:id', async (req, res) => {
     const { id } = req.params;
     const talker = talkerArray.find((item) => item.id === Number(id));
 
-    if(!talker) return res.status(HTTP_NOT_FOUND).json({  "message": "Pessoa palestrante não encontrada" });
+    if (!talker) {
+      return res.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+    }
 
     return res.status(HTTP_OK_STATUS).json(talker);
   } catch (error) {
