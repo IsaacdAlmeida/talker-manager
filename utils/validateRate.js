54e58dcda369fd8ab5e69316f22f9ecd2e9f7aff
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-02 20:25:53 -0300

    refactor(logic): change order on validateDate middleware to pass on test

--- utils/validateRate.js
+++ utils/validateRate.js
@@ -1,12 +1,11 @@
 const validateRate = (req, res, next) => {
   const { talk: { rate } } = req.body;
-
-  if (!rate || rate.length === 0) {
-    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  if (rate < 1 || rate > 5 || rate === '') {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
-  if (rate < 1 || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  if (!rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
 
   next();
